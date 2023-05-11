5d9d90444316f7bab4e56d995efa465f3a136563
Yaroslav <6715414@gmail.com>
2023-05-11 16:48:37 +0300

    added dotenv and to use cryptocurrency routes

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
-const axios = require("axios");
+const cryptocurrencyRoutes = require("./routes/cryptocurrencyRoutes.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+app.use("/", cryptocurrencyRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
