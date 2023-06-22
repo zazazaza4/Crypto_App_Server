8ee435bc7777c047b3e6e7f999e043e99c4a63fb
Yaroslav <6715414@gmail.com>
2023-06-23 00:34:58 +0300

    added middleware errorHandler and routes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const cryptocurrencyRoutes = require("./routes/cryptocurrencyRoutes.js");
+
+const coinsRoutes = require("./routes/coinsRoutes.js");
+const exchangeRoutes = require("./routes/exchangeRoutes.js");
+const errorHandler = require("./middlewares/errorHandler.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-app.use("/", cryptocurrencyRoutes);
+app.use("/coins", coinsRoutes);
+app.use("/exchanges", exchangeRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
