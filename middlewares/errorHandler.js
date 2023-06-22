01f0ccde89e6b597056bc56ede18c31615ed2dbe
Yaroslav <6715414@gmail.com>
2023-06-23 00:33:26 +0300

    added errorHandler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+function errorHandler(err, req, res, next) {
+  if (err.statusCode) {
+    res.status(err.statusCode).json({ error: err.message });
+  } else {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+module.exports = errorHandler;
